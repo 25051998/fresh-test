e3c228dd67bc60ad47a8faec0f24c6e17e4b6a38
25051998 <codelearner109@gmail.com>
2022-07-02 21:49:54 +0530

    added some custom message

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,8 +12,7 @@ export default function Home() {
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
-         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
+         Welcome to `fresh`. I(Aditya) was trying to see what fresh brings to the table. The deployment part is blazingly faast!!.
       </p>
       <Counter start={3} />
     </div>
